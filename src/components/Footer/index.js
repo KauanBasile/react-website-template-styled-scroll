7f3627e0e1a9f649f76231d21479d436cfedcb1a
Kauan Rodrigues <kauan.basile@gmail.com>
2022-07-06 10:23:51 -0300

    teste

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { animateScroll as scroll} from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
 import {FaFacebook, FaInstagram, FaYoutube, FaTwitter,
         FaLinkedin} from "react-icons/fa"
 import {
